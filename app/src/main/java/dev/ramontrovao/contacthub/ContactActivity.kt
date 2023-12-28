1b09d414f4557d6440af59a62994fd1c916f677b
Ramon Pinheiro Trovão da Silva <ramonzinhg12@gmail.com>
2023-12-28 20:54:25 -0300

    feat: added onItemClickListener on contact card

--- app/src/main/java/dev/ramontrovao/contacthub/ContactActivity.kt
+++ app/src/main/java/dev/ramontrovao/contacthub/ContactActivity.kt
@@ -1,11 +1,17 @@
 package dev.ramontrovao.contacthub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class ContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+
+        val contactId = intent.getLongExtra("contactId", 0)
+
+        println(contactId)
     }
 }
--- app/src/main/java/dev/ramontrovao/contacthub/MainActivity.kt
+++ app/src/main/java/dev/ramontrovao/contacthub/MainActivity.kt
@@ -2,8 +2,12 @@ package dev.ramontrovao.contacthub
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import dev.ramontrovao.contacthub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val contactsList = listOf(Contact(1, "Eu", R.drawable.contact1, "27995785613"))
@@ -12,14 +16,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val contactsAdapter = ContactsAdapter(this, contactsList)
         val contactsView = findViewById<ListView>(R.id.contactsView)
 
-        val contactsAdapter = ContactsAdapter(this, contactsList)
         contactsView.adapter = contactsAdapter
-
-        contactsView.setOnClickListener {
-            val intent = Intent(this@MainActivity, ContactActivity::class.java)
-            startActivity(intent)
+        contactsView.setOnItemClickListener {
+                parent, view, position, id ->
+                val intent = Intent(this, ContactActivity::class.java)
+                intent.putExtra("contactId", id)
+                startActivity(intent)
         }
     }
 }
