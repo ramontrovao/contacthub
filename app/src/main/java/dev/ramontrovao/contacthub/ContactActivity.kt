fa3331537b21658d28ac39c452818d94621c211b
Ramon Pinheiro Trovão da Silva <ramonzinhg12@gmail.com>
2023-12-28 22:02:07 -0300

    refactor: removed unused imports

--- app/src/main/java/dev/ramontrovao/contacthub/ContactActivity.kt
+++ app/src/main/java/dev/ramontrovao/contacthub/ContactActivity.kt
@@ -1,11 +1,9 @@
 package dev.ramontrovao.contacthub
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 
 class ContactActivity : AppCompatActivity() {
     private val contactsList = listOf(Contact(1, "Eu", R.drawable.contact1, "27995785613"))
--- app/src/main/java/dev/ramontrovao/contacthub/MainActivity.kt
+++ app/src/main/java/dev/ramontrovao/contacthub/MainActivity.kt
@@ -2,12 +2,8 @@ package dev.ramontrovao.contacthub
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.AdapterView.OnItemClickListener
 import android.widget.ListView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import dev.ramontrovao.contacthub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val contactsList = listOf(Contact(1, "Eu", R.drawable.contact1, "27995785613"))
