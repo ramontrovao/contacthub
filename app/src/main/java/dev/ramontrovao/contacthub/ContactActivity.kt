72d513b8a33cbf15712a434c2e574099f899e583
Ramon Pinheiro Trovão da Silva <ramonzinhg12@gmail.com>
2023-12-28 21:02:07 -0300

    feat: added contact image to ContactActivity

--- app/src/main/java/dev/ramontrovao/contacthub/ContactActivity.kt
+++ app/src/main/java/dev/ramontrovao/contacthub/ContactActivity.kt
@@ -6,12 +6,14 @@ import android.os.Bundle
 import android.widget.Toast
 
 class ContactActivity : AppCompatActivity() {
+    private val contactsList = listOf(Contact(1, "Eu", R.drawable.contact1, "27995785613"))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
 
         val contactId = intent.getLongExtra("contactId", 0)
+        val contact = contactsList.find { contact -> contact.getId() == contactId.toInt() }
 
-        println(contactId)
     }
 }
