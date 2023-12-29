77c1644c185c773e22ddb53bc715e610c57f01de
Ramon Pinheiro Trovão da Silva <ramonzinhg12@gmail.com>
2023-12-29 20:16:55 -0300

    refactor: removed listview and added recyclerview

--- app/src/main/java/dev/ramontrovao/contacthub/ContactsAdapter.kt
+++ app/src/main/java/dev/ramontrovao/contacthub/ContactsAdapter.kt
@@ -1,40 +1,49 @@
 package dev.ramontrovao.contacthub
 
-import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
-class ContactsAdapter(private val context: Context, private val contactList: List<Contact>): BaseAdapter() {
-    override fun getCount(): Int {
-        return contactList.size
+class ContactsAdapter(private val contactList: List<Contact>): RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val contactNameView: TextView
+        val contactPhoneView: TextView
+        val contactImageView: ImageView
+
+        init {
+            contactNameView = view.findViewById(R.id.contactName)
+            contactPhoneView = view.findViewById(R.id.contactNumber)
+            contactImageView = view.findViewById(R.id.contactImage)
+        }
     }
 
-    override fun getItem(position: Int): Contact {
-        return contactList[position]
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_card, parent, false)
+
+        return ViewHolder(view)
     }
 
-    override fun getItemId(position: Int): Long {
-
-        return contactList[position].getId().toLong()
-    }
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val inflater: LayoutInflater = LayoutInflater.from(context)
-        val view = inflater.inflate(R.layout.contact_card, parent, false)
-
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val contact = contactList[position]
 
-        val contactNameView = view?.findViewById<TextView>(R.id.contactName)
-        val contactImageView = view?.findViewById<ImageView>(R.id.contactImage)
-        val contactNumberView = view?.findViewById<TextView>(R.id.contactNumber)
-        contactNameView?.text = contact.getName()
-        contactNumberView?.text = contact.getPhoneNumber()
-        contactImageView?.setImageResource(contact.getImage())
+        holder.contactNameView.text = contact.getName()
+        holder.contactPhoneView.text = contact.getPhoneNumber()
+        holder.contactImageView.setImageResource(contact.getImage())
 
-        return view!!
+        holder.itemView.setOnClickListener {view ->
+            val intent = Intent(view.context, ContactActivity::class.java)
+            intent.putExtra("contactImage", contact.getImage())
+            intent.putExtra("contactName", contact.getName())
+            intent.putExtra("contactPhone", contact.getPhoneNumber())
+
+
+            view.context.startActivity(intent)
+        }
     }
+
+    override fun getItemCount(): Int = contactList.size
 }
--- app/src/main/java/dev/ramontrovao/contacthub/MainActivity.kt
+++ app/src/main/java/dev/ramontrovao/contacthub/MainActivity.kt
@@ -2,8 +2,9 @@ package dev.ramontrovao.contacthub
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,24 +28,16 @@ class MainActivity : AppCompatActivity() {
         val contactImages = listOf(R.drawable.contactman, R.drawable.contactgirl)
         val contactPhones = listOf("27995785114", "27995785114", "27994785231", "27991314159", "27991285093", "31992224093")
 
-        for (i in 1..50) {
-            val contactToAdd = Contact(i, contactNames[i - 1], contactImages.random(), contactPhones.random())
+        for (i in 1..10000) {
+            val contactToAdd = Contact(i, contactNames.random(), contactImages.random(), contactPhones.random())
 
             contactsList.add(contactToAdd)
         }
 
-        val contactsAdapter = ContactsAdapter(this, contactsList)
-        val contactsView = findViewById<ListView>(R.id.contactsView)
+        val contactsAdapter = ContactsAdapter(contactsList)
+        val contactsView = findViewById<RecyclerView>(R.id.contactsView)
 
+        contactsView.layoutManager = LinearLayoutManager(this)
         contactsView.adapter = contactsAdapter
-        contactsView.setOnItemClickListener {
-                parent, view, position, id ->
-                val intent = Intent(this, ContactActivity::class.java)
-                val contact = contactsList[position]
-                intent.putExtra("contactImage", contact.getImage())
-                intent.putExtra("contactName", contact.getName())
-                intent.putExtra("contactPhone", contact.getPhoneNumber())
-                startActivity(intent)
-        }
     }
 }
