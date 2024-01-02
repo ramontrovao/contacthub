43b02637a38891363408fca144c827bd4b0d797c
Ramon Pinheiro Trovão da Silva <ramonzinhg12@gmail.com>
2024-01-01 22:22:32 -0300

    feat: added actionbar to app

--- app/src/main/java/dev/ramontrovao/contacthub/ContactActivity.kt
+++ app/src/main/java/dev/ramontrovao/contacthub/ContactActivity.kt
@@ -2,17 +2,24 @@ package dev.ramontrovao.contacthub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class ContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+        setSupportActionBar(findViewById(R.id.contact_toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
 
         val contactImage = intent.getIntExtra("contactImage", 0)
         val contactName = intent.getStringExtra("contactName")
         val contactPhone = intent.getStringExtra("contactPhone")
+
+        supportActionBar?.title = contactName
 
         val contactImageView = findViewById<ImageView>(R.id.contactImage)
         val contactNameView = findViewById<TextView>(R.id.contactName)
@@ -21,5 +28,16 @@ class ContactActivity : AppCompatActivity() {
         contactImageView.setImageResource(contactImage)
         contactNameView.text = contactName
         contactPhoneView.text = contactPhone
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                this.finish()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
